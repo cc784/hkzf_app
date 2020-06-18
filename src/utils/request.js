1e2b486f89cf9b2db47e7c9b9435f008ddb21e8d
Cuar_ <1473815385@qq.com>
2020-06-18 19:20:37 +0800

    完善axios中拦截器的统一实现 加载中  效果
    如果同时发送出去了3个请求，那么应该是在最后一个请求回来的时候 才关闭 加载中

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,9 +4,12 @@ import axios from "axios";
 import { Toast} from 'antd-mobile';
 export const baseURL = 'http://157.122.54.189:9060';
 axios.defaults.baseURL = baseURL;
+
+let AjaxTimes=0;
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
+    AjaxTimes++; //拦截器的统一实现 加载中  效果
     Toast.loading('Loading...', 0); //加载提示
     return config;
   }, function (error) {
@@ -17,8 +20,11 @@ axios.interceptors.request.use(function (config) {
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
     // 对响应数据做点什么
-    setTimeout(function(){ Toast.hide(); },1000)
-     //隐藏 toast 需要手动调用 hide
+    AjaxTimes--;
+    if(AjaxTimes===0){
+      setTimeout(function(){ Toast.hide(); },1000)
+      //隐藏 toast 需要手动调用 hide
+    }
     return response;
   }, function (error) {
     // 对响应错误做点什么
