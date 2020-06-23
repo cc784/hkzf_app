6b563a2061f62ecfc03cec5b518e2b54e2867341
Cuar_ <1473815385@qq.com>
2020-06-23 14:40:15 +0800

    将城市输入组件-类组件替换成-函数式高性能组件

--- src/components/CityInput/index.js
+++ src/components/CityInput/index.js
@@ -6,55 +6,52 @@ import { connect } from "react-redux";
 import { getLocalCityAction } from '../../store/actionCreator';
 // 获取路由对象函数
 import {withRouter} from "react-router-dom"
-class index extends Component {
-  componentDidMount(){
-    this.props.initCity();
-  }
-  render() {
-    // console.log(this.props)
-    const {history}=this.props
-    return (
-      <div className={indexCss.city_input}>
-        <div className={indexCss.input_wrap}>
-          <div className={indexCss.city_label} onClick={()=>history.push('/citySelect')}>
-            <span>{this.props.cityName}</span>
-            <i className={
-              [
-                "iconfont",
-                "icon-arrow",
-                indexCss["icon-arrow"]
-              ].join(" ")
-            }></i>
-          </div>
-          <div className={indexCss.city_address}>
-            {/*  iconfont icon-seach 不能改 图标就无法显示 */}
-            {/*  */}
-            <i className={
-              [
-                "iconfont",
-                "icon-seach",
-                indexCss["icon-seach"]
-              ].join(" ")
-            }></i>
-            <span>请输入小区或地址</span>
-          </div>
-        </div>
-        <div className={indexCss.map_point}
-          onClick={()=>history.push('/mapFound')}
-        >
+
+// 函数式组件=》高性能组件
+const Index = React.memo((props)=>{
+  //  console.log(props)
+  const {history,cityName}=props;
+  return (
+    <div className={indexCss.city_input}>
+      <div className={indexCss.input_wrap}>
+        <div className={indexCss.city_label} onClick={()=>history.push('/citySelect')}>
+          <span>{cityName}</span>
           <i className={
             [
               "iconfont",
-              "icon-map",
-              indexCss["icon-map"]
+              "icon-arrow",
+              indexCss["icon-arrow"]
             ].join(" ")
           }></i>
         </div>
-
+        <div className={indexCss.city_address}>
+          {/*  iconfont icon-seach 不能改 图标就无法显示 */}
+          {/*  */}
+          <i className={
+            [
+              "iconfont",
+              "icon-seach",
+              indexCss["icon-seach"]
+            ].join(" ")
+          }></i>
+          <span>请输入小区或地址</span>
+        </div>
       </div>
-    );
-  }
-}
+      <div className={indexCss.map_point}
+        onClick={()=>history.push('/mapFound')}
+      >
+        <i className={
+          [
+            "iconfont",
+            "icon-map",
+            indexCss["icon-map"]
+          ].join(" ")
+        }></i>
+      </div>
+
+    </div>
+  );
+})
 // 负责将仓库中的数据映射到组件的props当中
 const mapStateToProps=(state)=>{
   return{
@@ -63,11 +60,11 @@ const mapStateToProps=(state)=>{
 }
 
 // 负责将 action 映射到组件的props中
-const mapDispathToprops = (dispath)=>{
-  return{
-    initCity(){
-      dispath(getLocalCityAction());
-    }
-  }
-}
-export default withRouter(connect(mapStateToProps,mapDispathToprops)(index));
+// const mapDispathToprops = (dispath)=>{
+//   return{
+//     initCity(){
+//       dispath(getLocalCityAction());
+//     }
+//   }
+// }
+export default withRouter(connect(mapStateToProps,null)(Index));
